b3dff6586f09d3c6920399d5d8414c46006483c2
HerdiEhzaSaputra <herdi.ehza@icloud.com>
2022-09-18 00:01:00 +0700

    remove preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import preact from "@astrojs/preact";
 import react from "@astrojs/react";
 
 import netlify from "@astrojs/netlify/functions";
@@ -9,7 +8,7 @@ import netlify from "@astrojs/netlify/functions";
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
   // integrations: [tailwind(), preact({ compat:true })],
-  integrations: [tailwind(), preact(), react()],
+  integrations: [tailwind(), react()],
   vite: {
     ssr: {
       external: ["@11ty/eleventy-img", "svgo"]
